af1496a79a74f02060f175a08b2beb71e91a4ae7
Shariq Shahbaz <iamsh4r10@gmail.com>
2020-02-21 17:06:31 +0100

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import MusicPlayer from './MusicPlayer';
+import '../App.css';
+
+function App(){
+        return(
+            <div className="main-container">
+                <MusicPlayer />
+            </div> 
+            )
+}
+
+export default App; 
---
+++ src/Components/MusicPlayer.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+
+class MusicPlayer extends Component{
+    constructor(){
+        super();
+        this.state = {
+            data: [],
+            nowPlaying: '',
+            previous: ''
+        }
+    }
+
+    componentDidMount = () =>{
+        $.get(
+          "https://www.googleapis.com/youtube/v3/playlistItems",
+          {
+            part: "snippet",
+            maxResults: 10,
+            playlistId: "PLx65qkgCWNJIs3FPaj8JZhduXSpQ_ZfvL",
+            key: "AIzaSyDmxZnPIkJrQzEg8dp7QY4aYJFr8wqwhdY"
+          },
+          data => {
+           const refined =  data.items.map(item => {
+                              return {
+                                title: item.snippet.title,
+                                thumbnail: item.snippet.thumbnails.high.url,
+                                videoId: item.snippet.resourceId.videoId
+                              }
+                            });
+            this.setState({ data: refined });
+            const random = Math.floor(Math.random() * this.state.data.length);
+            this.setState({nowPlaying: refined[random]});
+            console.log(refined);
+          }
+        );
+    }
+
+    render(){
+
+        return(
+          <div>
+            <p>Now Playing: {this.state.nowPlaying.title}</p>
+            <img src={this.state.nowPlaying.thumbnail} width={200}/>
+          </div>
+        )
+    }
+}
+
+export default MusicPlayer;
