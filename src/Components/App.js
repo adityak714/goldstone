df416b53e21474b89bd46d71a030f717374cefc4
Shariq Shahbaz <iamsh4r10@gmail.com>
2021-01-23 20:09:27 +0100

    added dark mode functionality

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,24 @@
-import React,{useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Main from "./Main";
 import Settings from "./Settings";
 
 const App = () => {
+    const [darkMode, setDarkmode] = useState(()=>{
+        if(localStorage.getItem("preferences")){
+            return JSON.parse(localStorage.getItem("preferences")).darkmode;
+        }
+        else{
+            if(!window.matchMedia) return false;
+            return window.matchMedia("prefers-color-scheme: dark").matches
+        }
+    });
 
     useEffect(()=>{
         const preferences= {
             playlistId: "PLx65qkgCWNJIs3FPaj8JZhduXSpQ_ZfvL",
             continue: false,
+            darkmode: darkMode,
         }
         if(!localStorage.getItem("preferences")){
             localStorage.setItem("preferences", JSON.stringify(preferences));
@@ -17,10 +27,10 @@ const App = () => {
 
     return (
         <Router>
-            <Route path="/" exact component={Main} />
-            <Route path="/settings" exact component={Settings} />
+            <Route path="/" exact component={()=> <Main darkmode={darkMode}/>} />
+            <Route path="/settings" exact component={()=><Settings darkmode={darkMode} setDarkmode={setDarkmode}/>} />
         </Router>
     )
 }
 
-export default App
+export default App;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,8 +5,8 @@ import {Link} from 'react-router-dom';
 import '../css/Main.css';
 
 export default class Main extends Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
             expanded: "",
             focusedTime: 0,
@@ -28,33 +28,27 @@ export default class Main extends Component {
         });
     }
 
-    getFocusedTime = (time) =>{
-        this.setState({focusedTime: time});
-    }
-
     render() {
         return (
-            <div>
-                <div className="main-container">
-                    <div className="heading">
-                        <h1>Goldstone.</h1>
-                        <div>
-                            <p>Time focused this week</p>
-                            <p>{this.state.focusedTime} minutes</p>
-                        </div>
+            <div className={this.props.darkmode ? "main-container dark-mode" : "main-container light-mode"}>
+                <div className="heading">
+                    <h1>Goldstone.</h1>
+                    <div className="focusedTime">
+                        <p>Time focused this week</p>
+                        <p>{this.state.focusedTime} minutes</p>
                     </div>
-                    <div className="bulk-container">
-                        <button id="toggle">
-                            <i className="material-icons-round">{this.state.expanded ? "chevron_left" : "queue_music"}</i>
-                        </button>
-                        <Timer sendFocusedTime={this.getFocusedTime} alertStatus={this.setAlertStatus}/>
-                        <MusicPlayer />   
-                    </div>
-                    <div className="footer">
-                        <a href="mailto:iamsh4r10@gmail.com" target="_blank"><i className="material-icons-round">email</i></a>
-                        <p>Copyright {new Date().getFullYear()}, <span><a rel="noopener noreferrer" href="https://github.com/sh4r10" target="_blank">SH4R10</a></span>, All Rights Reserved.</p>
-                        <Link id="settings" to="/settings"><i className="material-icons-round">settings</i></Link>
-                    </div>
+                </div>
+                <div className="bulk-container">
+                    <button id="toggle">
+                        <i className="material-icons-round">{this.state.expanded ? "chevron_left" : "queue_music"}</i>
+                    </button>
+                    <Timer />
+                    <MusicPlayer />   
+                </div>
+                <div className="footer">
+                    <a href="mailto:iamsh4r10@gmail.com" target="_blank"><i className="material-icons-round">email</i></a>
+                    <p>Copyright {new Date().getFullYear()}, <span><a rel="noopener noreferrer" href="https://github.com/sh4r10" target="_blank">SH4R10</a></span>, All Rights Reserved.</p>
+                    <Link id="settings" to="/settings"><i className="material-icons-round">settings</i></Link>
                 </div>
             </div>
         )
